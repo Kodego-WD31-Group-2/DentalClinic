f5052e3b06e3c899f2d4b2e96066fa16c35b40b4
huezxiii <huesli.olibyer.0134@gmail.com>
2023-02-13 01:15:54 +0800

    first commit

---
+++ .history/app/Http/Controllers/AuthController_20230213004934.php
@@ -0,0 +1,58 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Request\LoginRequest;
+use App\Http\Controllers\Controller;
+
+class AuthController extends Controller
+{
+    /**
+     * Show specified view.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function loginView()
+    {
+        return view('login.main', [
+            'layout' => 'login'
+        ]);
+    }
+
+    /**
+     * Authenticate login user.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function login(LoginRequest $request)
+    {
+        if (!\Auth::attempt([
+            'email' => $request->email,
+            'password' => $request->password
+        ])) {
+            throw new \Exception('Wrong email or password.');
+        }
+
+        
+    // dd('User authenticated');
+    // return redirect('/dashboard');
+
+    // dd('Redirect returned');
+
+        // return redirect('/dashboard'); //added this line
+    }
+
+    /**
+     * Logout user.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function logout()
+    {
+        \Auth::logout();
+        return redirect('login');
+    }
+}
