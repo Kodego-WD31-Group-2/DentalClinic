6fe147aa3986b93eec1a78f8c735472a8c8ce90c
ronsau27 <ronsau27@gmail.com>
2023-02-21 11:59:07 +0800

    RonRon

--- resources/views/partials/_hero.blade.php
+++ resources/views/partials/_hero.blade.php
@@ -13,18 +13,18 @@ style="
 >
   <div class="flex justify-center items-center h-full">
     <div class="text-center text-white px-6 md:px-12">
-      <h1 class="text-blue-200 text-5xl md:text-6xl xl:text-7xl font-bold tracking-tight mb-12">
+      <h1 class="text-blue-200 text-3xl md:text-2xl xl:text-7xl font-bold tracking-tight mb-6">
         Premier Dental Services made Accessible
       </h1>
-      <h3>Leading providers for your dental needs now more easy to reach</h3>
+      <h3 class="text-blue-200 text-3xl md:text-2xl xl:text-4xl font-bold tracking-tight mb-6">Leading providers for your dental needs now more easy to reach</h3>
       <br>
       <button
         type="button"
-        class="inline-block px-7 py-3 border-2 border-white text-white font-medium text-sm leading-snug uppercase rounded hover:bg-black hover:bg-opacity-5 focus:outline-none focus:ring-0 transition duration-150 ease-in-out"
-        data-mdb-ripple="true"
-        data-mdb-ripple-color="light"
+        class="inline-block px-7 py-3 m-5 border-2 border-white text-white font-medium text-sm leading-snug uppercase rounded hover:bg-custom-2 hover:bg-opacity-75 focus:outline-none focus:ring-0 transition duration-150 ease-in-out"
+          data-mdb-ripple="true"
+          data-mdb-ripple-color="light"
       >
-        Get started
+        Book An Appointment
       </button>
     </div>
   </div>
